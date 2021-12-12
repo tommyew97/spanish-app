4b6a392682505cbc15b4467e4a0aacb19ce0e8de
tommyew97 <tommy.woldseth@mazemap.com>
2021-12-12 18:23:18 +0100

    Added styled-components and removed some files

---
+++ src/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Button = styled.div`
+    width: 100px;
+    height: 60px;
+    background-color: #ff0000;
+    border-radius: 6px;
+`
+
+export const ButtonContainer = styled.div`
+    position: absolute;
+    top: 55%;
+    left: 20%;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: space-evenly;
+`
