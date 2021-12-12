9ec68205a263b1a0815e4723c0f31b5ed3b0fa99
Tommy Woldseth <tommyew7@gmail.com>
2021-12-12 20:00:30 +0100

    Basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import './App.css';
-import { Container, Button, ButtonContainer } from './styles';
+import "./App.css";
+import { Container, Button, ButtonContainer, QuestionText } from "./styles";
 
 function App() {
   return (
     <Container>
+      <QuestionText>What is "red" in spanish?</QuestionText>
       <ButtonContainer>
-        <Button/>
-        <Button/>
+        <div className="ButtonRow">
+          <Button color="#7F636E">
+            <span> Rojo </span>
+          </Button>
+          <Button color="#55868C">
+            <span> Rojo </span>
+          </Button>
+        </div>
+        <div className="ButtonRow">
+          <Button color="#594F3B">
+            <span> Rojo </span>
+          </Button>
+          <Button color="#2C5784">
+            <span> Rojo </span>
+          </Button>
+        </div>
       </ButtonContainer>
     </Container>
   );
--- src/styles.js
+++ src/styles.js
@@ -1,25 +1,48 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-    height: 100vh;
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  background-color: #fcfcfc;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Button = styled.div`
-    width: 100px;
-    height: 60px;
-    background-color: #ff0000;
-    border-radius: 6px;
-`
+  width: 150px;
+  height: 90px;
+  background-color: ${(props) => props.color};
+  border-radius: 6px;
+  box-shadow: rgba(0, 0, 0, 0.65) 2px 2px 2px 2px;
+  color: #fff;
+  font-size: 18px;
+  font-family: "Roboto", sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const ButtonContainer = styled.div`
-    position: absolute;
-    top: 55%;
-    left: 20%;
+  position: absolute;
+  top: 55%;
+  display: flex;
+  flex-direction: column;
+  height: 250px;
+  width: 100%;
+  justify-content: space-evenly;
+  .ButtonRow {
     display: flex;
+    width: 100%;
     justify-content: space-evenly;
-    align-items: space-evenly;
-`
+  }
+`;
+
+export const QuestionText = styled.h1`
+  font-family: "Roboto", sans-serif;
+  font-size: 28px;
+  color: #282828;
+  position: absolute;
+  top: 15%;
+  text-align: center;
+`;
