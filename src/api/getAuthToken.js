05b2da3dbe19bd9eb6719770173984468c3db52b
tommyew97 <tommyew7@gmail.com>
2021-12-19 14:52:27 +0100

    Made it possible to retrieve user data after login

---
+++ src/api/getAuthToken.js
@@ -0,0 +1,8 @@
+export async function getAuthToken() {
+    try {
+    const response = await fetch('https://spanish-app.azurewebsites.net/.auth/me');
+    return await response.json();
+    } catch(err) {
+        console.log(err)
+    }
+}
